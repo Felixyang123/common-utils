35c1ed332c7e0a840f862fdc6474c75b1281eeed
felix123 <870869261@qq.com>
2025-07-27 20:44:54 +0800

    feat(*): 新增多级缓存单测

--- src/test/java/com/lezai/common_utils/cache/CacheTest.java
+++ src/test/java/com/lezai/common_utils/cache/CacheTest.java
@@ -2,6 +2,7 @@ package com.lezai.common_utils.cache;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -10,9 +11,20 @@ public class CacheTest {
     @Autowired
     private UserCache userCache;
 
+    @Autowired
+    @Qualifier(value = "firstLevelCache")
+    private MultiCache<Object> firstLevelCache;
+
     @Test
     void userCache_test() {
         UserCache.User user = userCache.safetyGet("3", System.currentTimeMillis() + 1000 * 60 * 30);
+        System.out.println(user);
+    }
+
+    @Test
+    void multiCache_test() {
+        long ttl = System.currentTimeMillis() + 1000 * 60 * 30;
+        Object user = firstLevelCache.loadAndCache("3", ttl, key -> new UserCache.User("aaa"));
         System.out.println(user);
     }
 }
