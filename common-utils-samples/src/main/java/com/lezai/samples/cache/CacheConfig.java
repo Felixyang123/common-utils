4ae4c4d2cddda05bf1a9b945139fe285764874b8
wwx1442096 <wangyang574@h-partners.com>
2025-09-04 13:16:58 +0800

    feat(*): pom文件调整

--- common-utils-samples/src/main/java/com/lezai/samples/cache/CacheConfig.java
+++ common-utils-samples/src/main/java/com/lezai/samples/cache/CacheConfig.java
@@ -3,6 +3,7 @@ package com.lezai.samples.cache;
 import com.lezai.samples.cache.config.CacheProperties;
 import com.lezai.samples.cache.impl.MultiHashMapCacheManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +13,8 @@ public class CacheConfig {
     private CacheProperties cacheProperties;
 
     @Bean
-    public MultiHashMapCacheManager cacheManager(MultiCache<Object> l1Cache, MultiCache<Object> l2Cache) {
+    public MultiHashMapCacheManager cacheManager(@Qualifier(value = "l1Cache") MultiCache<Object> l1Cache,
+                                                 @Qualifier(value = "l2Cache") MultiCache<Object> l2Cache) {
         CacheProperties.GlobalCfg globalCfg = cacheProperties.getGlobalCfg();
         return new MultiHashMapCacheManager(l1Cache, l2Cache, globalCfg.getLocalCacheSize(), globalCfg.getLocalCacheTtl());
     }
